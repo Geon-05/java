8c60fd1f71e2612653ce590ad251bd907b0bb43c
geon <zqrc05@gmail.com>
2024-08-11 13:37:46 +0900

    test

--- SQL/사용자생성.sql
+++ SQL/사용자생성.sql
@@ -1,5 +1,5 @@
-create user user01 IDENTIFIED by 1234;
-grant connect, resource to user01;
+create user user02 IDENTIFIED by 1234;
+grant connect, resource to user02;
 
 -- 라이브러리 연습용
 create user LIBRARY_PRACTICE IDENTIFIED by 1234;
